298517f1a480b0a44a7f20eb13a9d4526c654384
Shifu Yee <shifuyee@github.com>
2017-10-20 23:04:37 -0400

    Added Events and Version files, refactored DiscordBot

--- discord_bot/discord_bot.py
+++ discord_bot/discord_bot.py
@@ -4,41 +4,10 @@ import requests
 import websockets
 
 from .opcodes import Opcodes
+from .events import Events
+from ._version import __version__
 
 CONFIG_FILE_PATH = 'configs/config.json'
-EVENTS = {1: 'READY',
-          2: 'RESUMED',
-          3: 'CHANNEL_CREATE',
-          4: 'CHANNEL_UPDATE',
-          5: 'CHANNEL_DELETE',
-          6: 'CHANNEL_PINS_UPDATE',
-          7: 'GUILD_CREATE',
-          8: 'GUILD_UPDATE',
-          9: 'GUILD_BAN_ADD',
-          10: 'GUILD_BAN_REMOVE',
-          11: 'GUILD_EMOJIS_UPDATE',
-          12: 'GUILD_INTEGRATIONS_UPDATE',
-          13: 'GUILD_MEMBER_ADD',
-          14: 'GUILD_MEMBER_REMOVE',
-          15: 'GUILD_MEMBER_UPDATE',
-          16: 'GUILD_MEMBERS_CHUNK',
-          17: 'GUILD_ROLE_CREATE',
-          18: 'GUILD_ROLE_UPDATE',
-          19: 'GUILD_ROLE_DELETE',
-          20: 'MESSAGE_CREATE',
-          21: 'MESSAGE_UPDATE',
-          22: 'MESSAGE_DELETE',
-          23: 'MESSAGE_DELETE_BULK',
-          24: 'MESSAGE_REACTION_ADD',
-          25: 'MESSAGE_REACTION_REMOVE',
-          26: 'MESSAGE_REACTION_REMOVE_ALL',
-          27: 'PRESENCE_UPDATE',
-          28: 'TYPING_START',
-          29: 'USER_UPDATE',
-          30: 'VOICE_STATE_UPDATE',
-          31: 'VOICE_SERVER_UPDATE',
-          32: 'WEBHOOKS_UPDATE'
-          }
 
 
 class DiscordBot:
@@ -49,7 +18,7 @@ class DiscordBot:
     def __init__(self):
         # instance variables
         self.config = self.load_config()
-        self.gateway_ws_url = self.get_gateway
+        self.gateway_ws_url = self.get_gateway()
         self.heartbeat_interval_ms = None
         self.last_seq = None
         self.event_loop = asyncio.get_event_loop()
@@ -69,7 +38,6 @@ class DiscordBot:
         with open(CONFIG_FILE_PATH) as f:
             return json.load(f)
 
-    @property
     def get_gateway(self):
         """
         Caches a gateway value, authenticates, and retrieves a new URL
@@ -78,11 +46,11 @@ class DiscordBot:
         header = {
             "headers": {
                 "Authorization": "Bot {}".format(self.config["handshake_identity"]["token"]),
-                "User-Agent": "DiscordBot (https://github.com/ShifuYee/Discord-Bot, 0.0.1)"
+                "User-Agent": "DiscordBot (https://github.com/ShifuYee/Discord-Bot, {})".format(__version__)
             }
         }
-        r = requests.get(self.config["discord_gateway_endpoint"], header)
-        assert (200 == r.status_code), r.reason
+        r = requests.get("{}/gateway".format(self.config["discord_api_endpoint"]), header)
+        assert 200 == r.status_code, r.reason
         return r.json()['url']
 
     async def send_json(self, payload):
@@ -99,15 +67,19 @@ class DiscordBot:
         asyncio.ensure_future(self.heartbeat())
         handshake_identity = self.config["handshake_identity"]
         if self.session_id:
-            asyncio.ensure_future(self.send_json(
-                                                {"op": Opcodes.RESUME,
-                                                  "d": {"token": self.config["handshake_identity"]["token"],
+            asyncio.ensure_future(self.send_json({
+                                                    "op": Opcodes.RESUME,
+                                                    "d": {
+                                                        "token": self.config["handshake_identity"]["token"],
                                                         "session_id": self.session_id,
                                                         "seq": self.last_seq
-                                                        }
-                                                 }))
+                                                    }
+            }))
         else:
-            asyncio.ensure_future(self.send_json({"op": Opcodes.IDENTIFY, "d": handshake_identity}))
+            asyncio.ensure_future(self.send_json({
+                                                    "op": Opcodes.IDENTIFY,
+                                                    "d": handshake_identity
+            }))
 
     async def heartbeat(self):
         """
@@ -115,7 +87,10 @@ class DiscordBot:
         """
         await asyncio.sleep(self.heartbeat_interval_ms / 1000.0)
         print("Last Sequence: {}".format(self.last_seq))
-        asyncio.ensure_future(self.send_json({"op": Opcodes.HEARTBEAT, "d": self.last_seq}))
+        asyncio.ensure_future(self.send_json({
+                                                "op": Opcodes.HEARTBEAT,
+                                                "d": self.last_seq
+        }))
         asyncio.ensure_future(self.heartbeat())
 
     async def gateway_handler(self):
@@ -130,21 +105,23 @@ class DiscordBot:
                 message = await self.websocket.recv()
                 message = json.loads(message)
                 print("{}: {}".format(Opcodes(message["op"]).name, message))
+                if message["s"] is not None:
+                    self.last_seq = message["s"]
+
                 if message["op"] == Opcodes.HELLO:
                     asyncio.ensure_future(self.handshake(message))
                 elif message["op"] == Opcodes.HEARTBEAT_ACK:
                     pass
                 elif message["op"] == Opcodes.INVALID_SESSION:
-                    self.last_seq = message["s"]
                     print('Invalid Session')
                 elif message["op"] == Opcodes.DISPATCH:
-                    self.last_seq = message["s"]
                     event = message["t"]
-                    if event == EVENTS[1]:
+                    if event == Events.READY:
                         self.session_id = message["d"]["session_id"]
                 else:
                     print(message)
 
+    # TODO: Remove this test code
     async def send_message(self, recipient_id, content):
         """
         Post a message into the given channel
@@ -152,8 +129,12 @@ class DiscordBot:
         :param content: obj - message sent
         :return: dict - Fires a 'Message Create' Gateway event
         """
-        channel = requests.post("{}", json={"recipient_id": recipient_id}).json()
+        channel = requests.post("{}", json={
+                                            "recipient_id": recipient_id
+                                            }).json()
 
-        return requests.post("{}{}/messages".format(self.config["discord_channel_endpoint"],
-                                                    channel["id"],
-                             json={"content": content})).json()
+        return requests.post("{}/channels/{}/messages".format(self.config["discord_api_endpoint"],
+                                                              channel["id"]),
+                             json={
+                                    "content": content
+                             }).json()
--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages
 setup(
     name="Momoi Bot",
-    version="0.0.1",
+    version=open("discord_bot/_version.py").readlines()[-1].split()[-1].strip("\"'"),
     packages=find_packages(),
     scripts=[],
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=['requests', 'asyncio', 'websockets'],
+    install_requires=['requests', 'asyncio', 'websockets', 'logging', 'aiohttp'],
 
     package_data={
-        '': {},
+        '': {
+
+        },
     },
 
     # metadata for upload to PyPI
